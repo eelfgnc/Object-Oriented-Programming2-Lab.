978daa2c95d81056155823e669858494a9caac79
eelfgnc <45571056+eelfgnc@users.noreply.github.com>
2019-09-02 17:17:36 +0300

    Add files via upload

---
+++ Lab 04/Quiz 01/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace quiz4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_adsoyad = new System.Windows.Forms.Label();
+            this.tx_adsoyad = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lb_adsoyad
+            // 
+            this.lb_adsoyad.AutoSize = true;
+            this.lb_adsoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lb_adsoyad.Location = new System.Drawing.Point(33, 45);
+            this.lb_adsoyad.Name = "lb_adsoyad";
+            this.lb_adsoyad.Size = new System.Drawing.Size(58, 15);
+            this.lb_adsoyad.TabIndex = 0;
+            this.lb_adsoyad.Text = "Ad Soyad";
+            // 
+            // tx_adsoyad
+            // 
+            this.tx_adsoyad.Location = new System.Drawing.Point(36, 71);
+            this.tx_adsoyad.Name = "tx_adsoyad";
+            this.tx_adsoyad.Size = new System.Drawing.Size(226, 20);
+            this.tx_adsoyad.TabIndex = 1;
+            this.tx_adsoyad.TextChanged += new System.EventHandler(this.tx_adsoyad_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(36, 106);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(102, 91);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cinsiyet";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(55, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Bayan";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(43, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Bay";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Location = new System.Drawing.Point(155, 106);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(107, 91);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Yas Grubu";
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(7, 66);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(74, 17);
+            this.radioButton5.TabIndex = 2;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Yaslı (>65)";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 43);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(98, 17);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Yetiskin (27-64)";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 20);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(81, 17);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = " Genc (<27)";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Listeye Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(326, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(329, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Cinsiyet Istatisligi";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(329, 261);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Yas Istatisligi";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 305);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tx_adsoyad);
+            this.Controls.Add(this.lb_adsoyad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_adsoyad;
+        private System.Windows.Forms.TextBox tx_adsoyad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Lab 04/Quiz 01/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quiz4
+{
+    public partial class Form1 : Form
+    {
+        string[] bilgi = new string[10];
+        string[] bilgiparcalari; 
+        string cinsiyett, yasGrubuu, adSoyadd;
+        int i = 0;
+        void MusteriEkle(String adSoyad, String cinsiyet, String yasGrubu)
+        {
+            bilgi[i] = adSoyad + "," + cinsiyet + "," + yasGrubu;
+        }
+        string YasGrubuIstatistigi()
+        {
+            double gnc, ytskn, ysl;
+            double genc = 0, yetiskin = 0, yasli = 0;
+            for (int k = 0; k < i; k++)
+            {
+                bilgiparcalari = bilgi[k].Split(',');
+                if (bilgiparcalari[2] == "Genc") genc++;
+                else if (bilgiparcalari[2] == "Yetiskin") yetiskin++;
+                else if (bilgiparcalari[2] == "Yasli") yasli++;
+            }
+            gnc = (double)(genc / i) * 100;
+            ytskn = (double)(yetiskin / i) * 100;
+            ysl = (double)(yasli / i) * 100;
+            string metin;
+            metin = "Grupta " + genc.ToString() + " Genç, " + yetiskin.ToString() + " Yetişkin, " + yasli.ToString() + " Yaslı Bulunmaktadir. Grubun %" +
+                     gnc.ToString("N2") + "'i genclerden, %" + ytskn.ToString("N2") + "'i yetiskinlerden, %" + ysl.ToString("N2") + "'i yaslilardan olusmaktadir.";
+            return metin;
+            
+        }
+        string CinsiyetIstatistigi()
+        {
+            double bayistatisligi, bayanistattisligi;
+            double byy = 0, bynn = 0;
+            for (int k = 0; k < i; k++)
+            {
+                bilgiparcalari = bilgi[k].Split(',');
+                if (bilgiparcalari[1] == "Bay") byy++;
+                else if (bilgiparcalari[1] == "Bayan") bynn++;
+            }
+            bayistatisligi = (double)(byy / i) * 100;
+            bayanistattisligi = (double)(bynn / i) * 100;
+            string metin;
+            metin = "Grupta " + byy.ToString() + " Bay, " + bynn.ToString() + " Bayan Bulunmaktadir. Grubun %" +
+                     bayistatisligi.ToString("N2") + "'ini Baylar, kalan %" + bayanistattisligi.ToString("N2") + "'ini Bayanlar olusturmaktadir.";
+            return metin;
+           
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string y = CinsiyetIstatistigi();
+            MessageBox.Show(y, "Cinsiyet Istatisligi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string x = YasGrubuIstatistigi();
+            MessageBox.Show(x, "Yas Grup Istatisligi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void tx_adsoyad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked) cinsiyett = "Bay";
+            else if (radioButton2.Checked) cinsiyett = "Bayan";
+
+            if (radioButton3.Checked) yasGrubuu = "Yetiskin";
+            else if (radioButton4.Checked) yasGrubuu = "Genc";
+            else if (radioButton5.Checked) yasGrubuu = "Yasli";
+
+            adSoyadd = tx_adsoyad.Text;
+            MusteriEkle(adSoyadd, cinsiyett, yasGrubuu);
+            label1.Text += bilgi[i].ToString() + Environment.NewLine;
+            i = i + 1;
+        }
+    }
+}
