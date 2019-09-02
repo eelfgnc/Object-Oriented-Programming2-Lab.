7a76696aadd4c9722c0054cfc4f185b22f808af8
eelfgnc <45571056+eelfgnc@users.noreply.github.com>
2019-09-02 17:15:20 +0300

    Add files via upload

---
+++ Lab 03/Quiz 01/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace quiz1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(208, 215);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(35, 13);
+            this.label.TabIndex = 0;
+            this.label.Text = "label1";
+            this.label.Click += new System.EventHandler(this.label_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 287);
+            this.Controls.Add(this.label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ Lab 03/Quiz 01/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quiz1
+{
+    public partial class Form1 : Form
+    {
+        TextBox[] array = new TextBox[5];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int min = 100, max = 0, sum = 0;
+            double ave = 0.0;
+            Random rndm = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                TextBox textBox = new TextBox();
+
+                int sayi = rndm.Next(0, 100);
+                textBox.Text = sayi.ToString(); array[i] = textBox;
+                textBox.Name = "textBox" + i.ToString(); 
+                textBox.Location = new Point(50 + i * 20, 50 + i * 30);
+                this.Controls.Add(textBox);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                sum += int.Parse(array[i].Text);
+               if (int.Parse(array[i].Text) < min)
+                {
+                    min = int.Parse(array[i].Text);
+                }
+                if (int.Parse(array[i].Text) > max)
+                {
+                    max = int.Parse(array[i].Text);
+                }
+            }
+            ave = (double) sum / 5;
+            label.Text = "min value: " + min.ToString() + "  max value: " + max.ToString() + "  average value: " + ave.ToString();
+
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
