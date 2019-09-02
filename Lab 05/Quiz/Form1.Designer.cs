7ec986cf5a0ba492f12643bb33ad7470cdecc0d6
eelfgnc <45571056+eelfgnc@users.noreply.github.com>
2019-09-02 17:20:17 +0300

    Add files via upload

---
+++ Lab 05/Quiz/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.rdb1 = new System.Windows.Forms.RadioButton();
+            this.rdb2 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(6, 19);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(377, 129);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(389, 167);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Listview-1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listView2);
+            this.groupBox2.Location = new System.Drawing.Point(422, 56);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(385, 167);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Listview-2";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(6, 19);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(362, 129);
+            this.listView2.TabIndex = 0;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // rdb1
+            // 
+            this.rdb1.AutoSize = true;
+            this.rdb1.Location = new System.Drawing.Point(18, 21);
+            this.rdb1.Name = "rdb1";
+            this.rdb1.Size = new System.Drawing.Size(77, 17);
+            this.rdb1.TabIndex = 3;
+            this.rdb1.Text = "Secenek 1";
+            this.rdb1.UseVisualStyleBackColor = true;
+            this.rdb1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rdb2
+            // 
+            this.rdb2.AutoSize = true;
+            this.rdb2.Location = new System.Drawing.Point(422, 21);
+            this.rdb2.Name = "rdb2";
+            this.rdb2.Size = new System.Drawing.Size(77, 17);
+            this.rdb2.TabIndex = 4;
+            this.rdb2.Text = "Secenek 2";
+            this.rdb2.UseVisualStyleBackColor = true;
+            this.rdb2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(270, 242);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 371);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rdb2);
+            this.Controls.Add(this.rdb1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.RadioButton rdb1;
+        private System.Windows.Forms.RadioButton rdb2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Lab 05/Quiz/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+        int[] A = new int[5];
+        int[] B = new int[5];
+
+        void mergewithunique(int[] a1, int[] a2)
+        {
+            string sayi;
+            int[] result = new int[a1.Length + a2.Length];
+            int listind = 0;
+            for (int i = 0; i < a1.Length; i++)
+            {
+                result[listind++] = a1[i];
+            }
+            for (int i = 0; i < a2.Length; i++)
+            {
+                result[listind++] = a2[i];
+            }
+            for (int i = 0; i < result.Length; i++)
+            {
+                int j;
+                for (j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i] == result[j])
+                    {
+                        break;
+                    }
+                    else if (j == result.Length-1)
+                    {
+                        listView1.Items.Add(result[i].ToString());
+                    }
+                }
+            }
+        }
+        void ciftler(int[] a1, int[] a2)
+        {
+            int[] result = new int[a1.Length + a2.Length];
+            int listind = 0;
+            for (int i = 0; i < a1.Length; ++i)
+            {
+                result[listind++] = a1[i];
+            }
+            for (int i = 0; i < a2.Length; ++i)
+            {
+                result[listind++] = a2[i];
+            }
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+                int j;
+                for (j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i] == result[j])
+                    {
+                        listView2.Items.Add(result[i].ToString());
+                        break;
+                    }
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+              mergewithunique(A, B);
+            listView2.Items.Clear();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            ciftler(A, B);
+            listView1.Items.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random temp = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                A[i] = temp.Next(0, 20);
+                B[i] = temp.Next(0, 20);
+                textBox1.Text += A[i].ToString() + "   ";
+            }
+            textBox1.Text += "   | | |    ";
+            for (int i = 0; i < 5; i++)
+            {
+                textBox1.Text += B[i].ToString() + "   ";
+            }
+
+        }
+      
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
